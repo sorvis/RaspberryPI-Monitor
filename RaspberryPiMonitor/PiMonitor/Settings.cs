e2250f930b1dabc8b1e10943ba2a709be8eb2c5e
Steven Orvis <steven@trappingpa.com>
2014-02-16 08:58:34 -0500

    Added Settings object with path for DS18B20 property

---
+++ RaspberryPiMonitor/PiMonitor/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PiMonitor
+{
+	public class Settings
+	{
+		public Settings ()
+		{
+		}
+
+		public List<string> TemperatureDS18B20Paths {
+			get;
+			set;
+		}
+	}
+}
+
